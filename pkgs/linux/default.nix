93d38b4fcb609c2e19b9a2080d017e045a92cccc
Ana Hobden <operator@hoverbear.org>
2021-11-28 09:18:07 -0800

    feat: rollback linux to 5.12.17 for zfs
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- pkgs/linux/default.nix
+++ pkgs/linux/default.nix
@@ -4,10 +4,10 @@ pkgs.buildLinux {
   src = pkgs.fetchFromGitHub {
     owner = "SolidRun";
     repo = "linux-stable";
-    rev = "50aac898777d5fa62130f5eca5546b637cb444e8";
-    sha256 = "iZInARLnOAjuzPaWu9ZHziQSubUHCNv2I8ZEAGrJJqI=";
+    rev = "0503edef26918141bd75b464d1e50a6d82cd569a";
+    sha256 = "UwqJ9AVzXYppfW5OPvtuvG36+gWhZ3/lVL6pIQw+5po=";
   };
-  version = "5.15.0";
+  version = "5.12.17";
   kernelPatches = [ ];
   structuredExtraConfig = with pkgs.lib.kernel; {
     CGROUP_FREEZER = yes;
